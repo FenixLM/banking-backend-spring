89b493f55b91a59e474619c7651abaf25d7f324d
Felix Lamadrid <felix.lamadrid97@gmail.com>
2026-03-15 02:17:44 -0500

    feat(BankingApplication): enable modular component scanning architecture

--- banking-app/src/main/java/com/felixlm/bankingapp/BankingApplication.java
+++ banking-app/src/main/java/com/felixlm/bankingapp/BankingApplication.java
@@ -3,10 +3,7 @@ package com.felixlm.bankingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.felixlm.usersmodule",
-        "com.felixlm.accountsmodule"
-})
+@SpringBootApplication(scanBasePackages = "com.felixlm")
 public class BankingApplication {
 
     public static void main(String[] args) {
